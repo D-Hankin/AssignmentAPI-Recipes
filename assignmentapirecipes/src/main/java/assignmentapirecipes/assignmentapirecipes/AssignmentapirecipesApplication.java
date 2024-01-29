15bcb72e0df935063a168b5153838f2139e78d30
David Hankin <d_hankin@outlook.com>
2024-01-29 10:10:25 +0100

    SpringBoot framework

---
+++ assignmentapirecipes/src/main/java/assignmentapirecipes/assignmentapirecipes/AssignmentapirecipesApplication.java
@@ -0,0 +1,13 @@
+package assignmentapirecipes.assignmentapirecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentapirecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentapirecipesApplication.class, args);
+	}
+
+}
